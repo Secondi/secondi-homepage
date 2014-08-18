cca44d9fc316780072f9aa579ca97f975f999639
Brendon John Muschamp <brendon@numerical.co.nz>
2014-08-19 11:35:20 +1200

    hello om

--- src/secondi_homepage/core.cljs
+++ src/secondi_homepage/core.cljs
@@ -1,4 +1,15 @@
-(ns secondi.core)
+(ns secondi.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
 (println "hello Secondi")
+
+(defonce app-state (atom 0))
+
+(defn secondi-app [app owner]
+  (om/component
+   (dom/div nil nil)))
+
+(om/root secondi-app app-state
+         {:target (. js/document (getElementById "page"))})
